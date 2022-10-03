de2ffac088fbd7148f5f2c6d6f88300596ab6c0d
javalosjr96 <jorge.avalos@liferay.com>
2022-10-02 18:57:34 -0700

    LPS-164521 buildService

--- modules/dxp/apps/saml/saml-persistence-service/src/main/resources/META-INF/sql/tables.sql
+++ modules/dxp/apps/saml/saml-persistence-service/src/main/resources/META-INF/sql/tables.sql
@@ -48,12 +48,12 @@ create table SamlPeerBinding (
 	userId LONG,
 	userName VARCHAR(75) null,
 	deleted BOOLEAN,
-	samlNameIdFormat VARCHAR(1024) null,
+	samlNameIdFormat VARCHAR(75) null,
 	samlNameIdNameQualifier VARCHAR(75) null,
 	samlNameIdSpNameQualifier VARCHAR(75) null,
 	samlNameIdSpProvidedId VARCHAR(75) null,
-	samlNameIdValue VARCHAR(1024) null,
-	samlPeerEntityId VARCHAR(1024) null
+	samlNameIdValue VARCHAR(75) null,
+	samlPeerEntityId VARCHAR(75) null
 );
 
 create table SamlSpAuthRequest (
