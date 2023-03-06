423b7e170f145a0d62ec5edef48631e6b118387c
Jorge A Avalos Jr <jorge.avalos@liferay.com>
2023-03-05 22:29:30 -0800

    LPS-174140 Add try/catch for SQL exception

--- modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/registry/UpgradeStepRegistratorTracker.java
+++ modules/apps/portal/portal-upgrade-impl/src/main/java/com/liferay/portal/upgrade/internal/registry/UpgradeStepRegistratorTracker.java
@@ -35,6 +35,8 @@ import com.liferay.portal.util.PropsValues;
 
 import java.io.OutputStream;
 
+import java.sql.SQLException;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -177,8 +179,14 @@ public class UpgradeStepRegistratorTracker {
 					bundleSymbolicName, releaseUpgradeSteps);
 			}
 
-			List<UpgradeInfo> upgradeInfos =
-				upgradeStepRegistry.getUpgradeInfos();
+			List<UpgradeInfo> upgradeInfos = null;
+
+			try {
+				upgradeInfos = upgradeStepRegistry.getUpgradeInfos();
+			}
+			catch (SQLException sqlException) {
+				throw new RuntimeException(sqlException);
+			}
 
 			if (PropsValues.UPGRADE_DATABASE_AUTO_RUN ||
 				(_releaseLocalService.fetchRelease(bundleSymbolicName) ==
