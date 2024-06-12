be0976fabaa7e4df2ce5677e97f46b66d5354ac8
Jorge Avalos <jorge.avalos@liferay.com>
2024-06-11 23:26:29 -0700

    Revert "LPD-27050 Simplify"
    This reverts commit 8fa39ec99d8c9e84396f60a7958874a32f545ff5.

--- modules/apps/portal-scheduler/portal-scheduler-quartz/src/main/java/com/liferay/portal/scheduler/quartz/internal/upgrade/v1_0_3/QuartzUpgradeProcess.java
+++ modules/apps/portal-scheduler/portal-scheduler-quartz/src/main/java/com/liferay/portal/scheduler/quartz/internal/upgrade/v1_0_3/QuartzUpgradeProcess.java
@@ -5,10 +5,8 @@
 
 package com.liferay.portal.scheduler.quartz.internal.upgrade.v1_0_3;
 
-import com.liferay.petra.string.CharPool;
 import com.liferay.portal.kernel.db.partition.DBPartition;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.StringUtil;
 
 /**
  * @author Mariano Álvaro Sáiz
@@ -21,50 +19,111 @@ public class QuartzUpgradeProcess extends UpgradeProcess {
 			return;
 		}
 
-		for (String createIndexSQLStatement : _CREATE_INDEX_SQL_STATEMENTS) {
-			_addIndex(createIndexSQLStatement);
+		if (!hasIndex("QUARTZ_JOB_DETAILS", "IX_88328984")) {
+			runSQLTemplateString(
+				"create index IX_88328984 on QUARTZ_JOB_DETAILS (SCHED_NAME, " +
+					"JOB_GROUP);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_JOB_DETAILS", "IX_779BCA37")) {
+			runSQLTemplateString(
+				"create index IX_779BCA37 on QUARTZ_JOB_DETAILS (SCHED_NAME, " +
+					"REQUESTS_RECOVERY);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_FIRED_TRIGGERS", "IX_BE3835E5")) {
+			runSQLTemplateString(
+				"create index IX_BE3835E5 on QUARTZ_FIRED_TRIGGERS (" +
+					"SCHED_NAME, TRIGGER_NAME, TRIGGER_GROUP);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_FIRED_TRIGGERS", "IX_4BD722BM")) {
+			runSQLTemplateString(
+				"create index IX_4BD722BM on QUARTZ_FIRED_TRIGGERS (" +
+					"SCHED_NAME, TRIGGER_GROUP);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_FIRED_TRIGGERS", "IX_339E078M")) {
+			runSQLTemplateString(
+				"create index IX_339E078M on QUARTZ_FIRED_TRIGGERS (" +
+					"SCHED_NAME, INSTANCE_NAME, REQUESTS_RECOVERY);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_FIRED_TRIGGERS", "IX_5005E3AF")) {
+			runSQLTemplateString(
+				"create index IX_5005E3AF on QUARTZ_FIRED_TRIGGERS (" +
+					"SCHED_NAME, JOB_NAME, JOB_GROUP);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_FIRED_TRIGGERS", "IX_BC2F03B0")) {
+			runSQLTemplateString(
+				"create index IX_BC2F03B0 on QUARTZ_FIRED_TRIGGERS (" +
+					"SCHED_NAME, JOB_GROUP);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_TRIGGERS", "IX_91CA7CCE")) {
+			runSQLTemplateString(
+				"create index IX_91CA7CCE on QUARTZ_TRIGGERS (SCHED_NAME, " +
+					"TRIGGER_GROUP, NEXT_FIRE_TIME, TRIGGER_STATE, " +
+						"MISFIRE_INSTR);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_TRIGGERS", "IX_D219AFDE")) {
+			runSQLTemplateString(
+				"create index IX_D219AFDE on QUARTZ_TRIGGERS (SCHED_NAME, " +
+					"TRIGGER_GROUP, TRIGGER_STATE);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_TRIGGERS", "IX_A85822A0")) {
+			runSQLTemplateString(
+				"create index IX_A85822A0 on QUARTZ_TRIGGERS (SCHED_NAME, " +
+					"JOB_NAME, JOB_GROUP);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_TRIGGERS", "IX_8AA50BE1")) {
+			runSQLTemplateString(
+				"create index IX_8AA50BE1 on QUARTZ_TRIGGERS (SCHED_NAME, " +
+					"JOB_GROUP);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_TRIGGERS", "IX_F2DD7C7E")) {
+			runSQLTemplateString(
+				"create index IX_F2DD7C7E on QUARTZ_TRIGGERS (SCHED_NAME, " +
+					"NEXT_FIRE_TIME, TRIGGER_STATE, MISFIRE_INSTR);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_TRIGGERS", "IX_1F92813C")) {
+			runSQLTemplateString(
+				"create index IX_1F92813C on QUARTZ_TRIGGERS (SCHED_NAME, " +
+					"NEXT_FIRE_TIME, MISFIRE_INSTR);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_TRIGGERS", "IX_99108B6E")) {
+			runSQLTemplateString(
+				"create index IX_99108B6E on QUARTZ_TRIGGERS (SCHED_NAME, " +
+					"TRIGGER_STATE);",
+				false);
+		}
+
+		if (!hasIndex("QUARTZ_TRIGGERS", "IX_CD7132D0")) {
+			runSQLTemplateString(
+				"create index IX_CD7132D0 on QUARTZ_TRIGGERS (SCHED_NAME, " +
+					"CALENDAR_NAME);",
+				false);
 		}
 	}
-
-	private void _addIndex(String createIndexSQLStatement) throws Exception {
-		String[] parts = StringUtil.split(
-			createIndexSQLStatement, CharPool.SPACE);
-
-		if (!hasIndex(parts[2], parts[4])) {
-			runSQL(createIndexSQLStatement);
-		}
-	}
-
-	private static final String[] _CREATE_INDEX_SQL_STATEMENTS = {
-		"CREATE INDEX IX_4BD722BM ON QUARTZ_FIRED_TRIGGERS (SCHED_NAME, " +
-			"TRIGGER_GROUP)",
-		"CREATE INDEX IX_339E078M ON QUARTZ_FIRED_TRIGGERS (SCHED_NAME, " +
-			"INSTANCE_NAME, REQUESTS_RECOVERY)",
-		"CREATE INDEX IX_5005E3AF ON QUARTZ_FIRED_TRIGGERS (SCHED_NAME, " +
-			"JOB_NAME, JOB_GROUP)",
-		"CREATE INDEX IX_BC2F03B0 ON QUARTZ_FIRED_TRIGGERS (SCHED_NAME, " +
-			"JOB_GROUP)",
-		"CREATE INDEX IX_BE3835E5 ON QUARTZ_FIRED_TRIGGERS (SCHED_NAME, " +
-			"TRIGGER_NAME, TRIGGER_GROUP)",
-		"CREATE INDEX IX_CD7132D0 ON QUARTZ_TRIGGERS (SCHED_NAME, " +
-			"CALENDAR_NAME)",
-		"CREATE INDEX IX_F2DD7C7E ON QUARTZ_TRIGGERS (SCHED_NAME, " +
-			"NEXT_FIRE_TIME, TRIGGER_STATE, MISFIRE_INSTR)",
-		"CREATE INDEX IX_1F92813C ON QUARTZ_TRIGGERS (SCHED_NAME, " +
-			"NEXT_FIRE_TIME, MISFIRE_INSTR)",
-		"CREATE INDEX IX_A85822A0 ON QUARTZ_TRIGGERS (SCHED_NAME, JOB_NAME, " +
-			"JOB_GROUP)",
-		"CREATE INDEX IX_8AA50BE1 ON QUARTZ_TRIGGERS (SCHED_NAME, JOB_GROUP)",
-		"CREATE INDEX IX_91CA7CCE ON QUARTZ_TRIGGERS (SCHED_NAME, " +
-			"TRIGGER_GROUP, NEXT_FIRE_TIME, TRIGGER_STATE, MISFIRE_INSTR)",
-		"CREATE INDEX IX_D219AFDE ON QUARTZ_TRIGGERS (SCHED_NAME, " +
-			"TRIGGER_GROUP, TRIGGER_STATE)",
-		"CREATE INDEX IX_99108B6E ON QUARTZ_TRIGGERS (SCHED_NAME, " +
-			"TRIGGER_STATE)",
-		"CREATE INDEX IX_88328984 ON QUARTZ_JOB_DETAILS (SCHED_NAME, " +
-			"JOB_GROUP)",
-		"CREATE INDEX IX_779BCA37 ON QUARTZ_JOB_DETAILS (SCHED_NAME, " +
-			"REQUESTS_RECOVERY)"
-	};
 
 }
