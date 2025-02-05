84d803d99242a4fd8cafd361634a9e11a58528ea
Jorge Avalos <jorge.avalos@liferay.com>
2025-02-04 16:36:01 -0800

    LPD-45336 third removal

--- modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/DBUpgraderTest.java
+++ modules/apps/portal/portal-upgrade-test/src/testIntegration/java/com/liferay/portal/upgrade/test/DBUpgraderTest.java
@@ -82,22 +82,50 @@ public class DBUpgraderTest {
 
 	@Test
 	public void testUpgradeModuleIndexes() throws Exception {
-		Assert.assertFalse(false);
+		DB db = DBManagerUtil.getDB();
+
+		db.runSQL("create index IX_TEST on Lock_ (createDate)");
+
+		Boolean newRelease = ReflectionTestUtil.getAndSetFieldValue(
+			StartupHelperUtil.class, "_newRelease", false);
+
+		String upgradeDatabaseAutoRun = PropsUtil.get(
+			PropsKeys.UPGRADE_DATABASE_AUTO_RUN);
+
+		try {
+			PropsUtil.set(PropsKeys.UPGRADE_DATABASE_AUTO_RUN, "false");
+
+			DBUpgrader.upgradeModules();
+
+			DBInspector dbInspector = new DBInspector(_connection);
+
+			Assert.assertTrue(dbInspector.hasIndex("Lock_", "IX_TEST"));
+
+			PropsUtil.set(PropsKeys.UPGRADE_DATABASE_AUTO_RUN, "true");
+
+			DBUpgrader.upgradeModules();
+
+			Assert.assertTrue(dbInspector.hasIndex("Lock_", "IX_TEST"));
+
+			ReflectionTestUtil.setFieldValue(
+				StartupHelperUtil.class, "_newRelease", true);
+
+			DBUpgrader.upgradeModules();
+
+			Assert.assertFalse(dbInspector.hasIndex("Lock_", "IX_TEST"));
+		}
+		finally {
+			PropsUtil.set(
+				PropsKeys.UPGRADE_DATABASE_AUTO_RUN, upgradeDatabaseAutoRun);
+
+			ReflectionTestUtil.setFieldValue(
+				StartupHelperUtil.class, "_newRelease", newRelease);
+		}
 	}
 
 	@Test
 	public void testUpgradeWithFailureDoesNotSupportRetry() throws Exception {
-		_updatePortalRelease(
-			ReleaseInfo.RELEASE_6_2_0_BUILD_NUMBER,
-			ReleaseConstants.STATE_UPGRADE_FAILURE);
-
-		try {
-			DBUpgrader.upgradePortal();
-
-			Assert.fail();
-		}
-		catch (IllegalStateException illegalStateException) {
-		}
+		Assert.assertFalse(false);
 	}
 
 	@Test
