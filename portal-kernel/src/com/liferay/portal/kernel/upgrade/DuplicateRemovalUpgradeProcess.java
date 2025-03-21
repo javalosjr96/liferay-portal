bdd704314a1f7407c2aec164c233a3d3b11b4fb9
Alberto Chaparro <alberto.chaparro@liferay.com>
2025-03-21 14:04:35 +0100

    LPD-47268 Now it is an array we do not need to split it

--- portal-kernel/src/com/liferay/portal/kernel/upgrade/DuplicateRemovalUpgradeProcess.java
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/DuplicateRemovalUpgradeProcess.java
@@ -36,7 +36,7 @@ public class DuplicateRemovalUpgradeProcess extends UpgradeProcess {
 		String tableName, String[] columnNames, String orderByClause) {
 
 		_tableName = tableName;
-		_columnNames = String.join(", ", columnNames);
+		_columnNames = columnNames;
 		_orderByClause = orderByClause;
 	}
 
@@ -50,16 +50,14 @@ public class DuplicateRemovalUpgradeProcess extends UpgradeProcess {
 
 		List<Map<String, String>> queryResult = new ArrayList<>();
 
-		String[] columns = _columnNames.split(", ");
-
 		StringBundler sb = new StringBundler();
 
 		sb.append("select * from ");
 		sb.append(_tableName);
 		sb.append(" where ");
 
-		for (int i = 0; i < indexDuplicateColumns.length; i++) {
-			sb.append(columns[i]);
+		for (int i = 0; i < _columnNames.length; i++) {
+			sb.append(_columnNames[i]);
 
 			if (indexDuplicateColumns[i] == null) {
 				sb.append(" IS NULL ");
@@ -70,7 +68,7 @@ public class DuplicateRemovalUpgradeProcess extends UpgradeProcess {
 				sb.append("' ");
 			}
 
-			if (i < (columns.length - 1)) {
+			if (i < (_columnNames.length - 1)) {
 				sb.append("and ");
 			}
 		}
@@ -135,11 +133,11 @@ public class DuplicateRemovalUpgradeProcess extends UpgradeProcess {
 		StringBundler sb = new StringBundler(7);
 
 		sb.append("select ");
-		sb.append(_columnNames);
+		sb.append(String.join(", ", _columnNames));
 		sb.append(" from ");
 		sb.append(_tableName);
 		sb.append(" group by ");
-		sb.append(_columnNames);
+		sb.append(String.join(", ", _columnNames));
 		sb.append(" having count(*) > 1");
 
 		String sql = sb.toString();
@@ -178,7 +176,8 @@ public class DuplicateRemovalUpgradeProcess extends UpgradeProcess {
 			_log.warn(
 				StringBundler.concat(
 					"Deleted duplicate entry from ", _tableName,
-					" table for index columns (", _columnNames, "): ",
+					" table for index columns (",
+					String.join(", ", _columnNames), "): ",
 					duplicate.toString()));
 		}
 	}
@@ -238,7 +237,7 @@ public class DuplicateRemovalUpgradeProcess extends UpgradeProcess {
 						StringBundler.concat(
 							"Failed to remove duplicate entry: ",
 							duplicate.toString(), " in ", _tableName, " for ",
-							_columnNames),
+							String.join(", ", _columnNames)),
 						sqlException);
 				}
 				finally {
@@ -256,7 +255,7 @@ public class DuplicateRemovalUpgradeProcess extends UpgradeProcess {
 	private static final Log _log = LogFactoryUtil.getLog(
 		DuplicateRemovalUpgradeProcess.class);
 
-	private final String _columnNames;
+	private final String[] _columnNames;
 	private final String _orderByClause;
 	private final String _tableName;
 
