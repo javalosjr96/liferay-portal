dca15517d393edbbd86b75eb73882b3108fbeb9c
Jorge Avalos <jorge.avalos@liferay.com>
2026-01-07 22:05:55 -0800

    LPD-75034 implement deleteCompany in DBStore and CTStore

--- modules/apps/portal-store/portal-store-db/src/main/java/com/liferay/portal/store/db/DBStore.java
+++ modules/apps/portal-store/portal-store-db/src/main/java/com/liferay/portal/store/db/DBStore.java
@@ -12,8 +12,12 @@ import com.liferay.document.library.kernel.exception.NoSuchFileException;
 import com.liferay.document.library.kernel.store.Store;
 import com.liferay.document.library.kernel.util.DLUtil;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 
 import java.io.InputStream;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import java.util.Arrays;
 import java.util.List;
@@ -40,6 +44,18 @@ public class DBStore implements Store {
 
 		_dlContentLocalService.addContent(
 			companyId, repositoryId, fileName, versionLabel, inputStream);
+	}
+
+	@Override
+	public void deleteCompany(long companyId) throws Exception {
+		try (Connection connection = DataAccess.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(
+				"delete from DLContent where companyId = ?")) {
+
+			preparedStatement.setLong(1, companyId);
+
+			preparedStatement.executeUpdate();
+		}
 	}
 
 	@Override
