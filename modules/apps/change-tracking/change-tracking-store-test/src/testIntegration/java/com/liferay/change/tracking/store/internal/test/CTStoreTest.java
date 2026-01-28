2a443970cc019cfb98d6cca488342e2f4c10df93
Jorge Avalos <jorge.avalos@liferay.com>
2026-01-28 13:26:47 -0800

    LPD-75040 add integration test

--- modules/apps/change-tracking/change-tracking-store-test/src/testIntegration/java/com/liferay/change/tracking/store/internal/test/CTStoreTest.java
+++ modules/apps/change-tracking/change-tracking-store-test/src/testIntegration/java/com/liferay/change/tracking/store/internal/test/CTStoreTest.java
@@ -28,6 +28,7 @@ import com.liferay.petra.string.StringUtil;
 import com.liferay.portal.change.tracking.store.CTStoreFactory;
 import com.liferay.portal.kernel.change.tracking.CTCollectionThreadLocal;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.instance.PortalInstancePool;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
@@ -85,6 +86,9 @@ public class CTStoreTest {
 		for (int i = 0; i < 4; i++) {
 			_ctCollections[i] = _createCTCollection();
 		}
+
+		_fileSystemStore.deleteDirectory(
+			_companyId, _REPOSITORY_ID, StringPool.BLANK);
 	}
 
 	@After
@@ -201,6 +205,39 @@ public class CTStoreTest {
 
 		_assertNoSuchCTSContent(fileName);
 		_assertFile(fileName, _DATA_3);
+	}
+
+	@Test
+	public void testGetCompanyIds() throws Exception {
+
+		// Production mode, with files
+
+		_addFiles("testDir1/testFile1:v1");
+
+		Assert.assertArrayEquals(PortalInstancePool.getCompanyIds(), _getCompanyIds());
+
+		_assertMethods(_GET_COMPANY_IDS_METHOD);
+
+		// CT mode, delete company files
+
+		_deleteDirectory();
+
+		_assertMethods(_DELETE_COMPANY_METHOD);
+
+		String fileName = "testFile";
+
+		_runInCTMode(
+			_ctCollections[0],
+			() -> {
+				_addCTFile(fileName, _DATA_1);
+
+				_assertCTSContent(fileName, _DATA_1);
+				_assertNoSuchFile(fileName);
+
+				Assert.assertArrayEquals(PortalInstancePool.getCompanyIds(),_getCompanyIds());
+			});
+
+		_assertNoSuchFile(fileName);
 	}
 
 	@Test
@@ -1071,6 +1108,10 @@ public class CTStoreTest {
 			_companyId, _REPOSITORY_ID, fileName, version);
 	}
 
+	private long[] _getCompanyIds() throws Exception {
+		return _ctStore.getCompanyIds();
+	}
+
 	private void _publish(CTCollection ctCollection) throws Exception {
 		try (SafeCloseable safeCloseable =
 				CTCollectionThreadLocal.setCTCollectionIdWithSafeCloseable(
@@ -1304,6 +1345,8 @@ public class CTStoreTest {
 
 	private static final Method _DELETE_FILE_METHOD;
 
+	private static final Method _GET_COMPANY_IDS_METHOD;
+
 	private static final Method _GET_FILE_AS_STREAM_METHOD;
 
 	private static final Method _GET_FILE_NAMES;
@@ -1358,6 +1401,8 @@ public class CTStoreTest {
 
 			_DELETE_DIRECTORY_METHOD = Store.class.getMethod(
 				"deleteDirectory", long.class, long.class, String.class);
+
+			_GET_COMPANY_IDS_METHOD = Store.class.getMethod("getCompanyIds");
 
 			_DELETE_FILE_METHOD = Store.class.getMethod(
 				"deleteFile", long.class, long.class, String.class,
