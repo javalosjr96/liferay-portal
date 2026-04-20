a3f9e2eeefee3590b996ea21e351ee81c4d5b646
Jorge Avalos <jorge.avalos@liferay.com>
2026-04-20 10:33:44 -0700

    LPD-84024 Define Database Monitoring API for Locked Queries

--- portal-kernel/src/com/liferay/portal/kernel/dao/db/DB.java
+++ portal-kernel/src/com/liferay/portal/kernel/dao/db/DB.java
@@ -15,6 +15,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -81,6 +82,16 @@ public interface DB {
 	public List<IndexMetadata> dropIndexes(
 			Connection connection, String tableName, String columnName)
 		throws IOException, SQLException;
+
+	public default List<RunningQuery> getActiveQueries(Connection connection)
+		throws Exception {
+
+		return Collections.emptyList();
+	}
+
+	public default String getActiveQueriesSQL() {
+		return null;
+	}
 
 	public String getCharacterSet(Connection connection) throws SQLException;
 
@@ -239,5 +250,52 @@ public interface DB {
 			Connection connection, String tableName,
 			String[] primaryKeyColumnNames)
 		throws Exception;
+
+	public class RunningQuery {
+
+		public RunningQuery(
+			long duration, String id, boolean locked, String query,
+			String schema, String state) {
+
+			_duration = duration;
+			_id = id;
+			_locked = locked;
+			_query = query;
+			_schema = schema;
+			_state = state;
+		}
+
+		public long getDuration() {
+			return _duration;
+		}
+
+		public String getId() {
+			return _id;
+		}
+
+		public String getQuery() {
+			return _query;
+		}
+
+		public String getSchema() {
+			return _schema;
+		}
+
+		public String getState() {
+			return _state;
+		}
+
+		public boolean isLocked() {
+			return _locked;
+		}
+
+		private final long _duration;
+		private final String _id;
+		private final boolean _locked;
+		private final String _query;
+		private final String _schema;
+		private final String _state;
+
+	}
 
 }
