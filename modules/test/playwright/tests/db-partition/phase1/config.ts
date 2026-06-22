b368f0aaeee5cdc705ffa2a2c953c7658f31af1d
Jorge Avalos <jorge.avalos@liferay.com>
2026-06-22 02:27:51 -0700

    LPD-91814 SF

--- modules/test/playwright/tests/db-partition/phase1/config.ts
+++ modules/test/playwright/tests/db-partition/phase1/config.ts
@@ -3,17 +3,9 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-export const dbPartitionPhase1Config = {
+export const config = {
 	name: 'db-partition.phase1',
 	testDir: 'tests/db-partition/phase1',
-	use: {
-		testIdAttribute: 'data-qa-id',
-	},
-};
-
-export const dbPartitionPhase2Config = {
-	name: 'db-partition.phase2',
-	testDir: 'tests/db-partition/phase2',
 	use: {
 		testIdAttribute: 'data-qa-id',
 	},
