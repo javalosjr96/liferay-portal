cb51327817a0b79d73e678523a1f17e581923eae
Jorge Avalos <jorge.avalos@liferay.com>
2026-06-30 16:38:52 -0700

    LPD-96642 Drop redundant Page import from portal smoke upgrade spec

--- modules/test/playwright/tests/portal-db-infrastructure/upgrade/portalSmokeUpgrade.spec.ts
+++ modules/test/playwright/tests/portal-db-infrastructure/upgrade/portalSmokeUpgrade.spec.ts
@@ -3,13 +3,13 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {Page, expect, mergeTests} from '@playwright/test';
+import {expect, mergeTests} from '@playwright/test';
 
 import {loginTest} from '../../../fixtures/loginTest';
 
 const test = mergeTests(loginTest());
 
-async function viewUpgradedPortalContent(page: Page) {
+async function viewUpgradedPortalContent(page) {
 	await test.step('View web content after upgrade', async () => {
 		await page.goto('/web/guest/web-content');
 
