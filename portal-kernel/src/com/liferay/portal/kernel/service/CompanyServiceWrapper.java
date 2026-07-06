192590773059252c724720aef1e3554547bde425
Jorge Avalos <jorge.avalos@liferay.com>
2026-07-06 10:43:04 -0700

    LPD-92618 buildService

--- portal-kernel/src/com/liferay/portal/kernel/service/CompanyServiceWrapper.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/CompanyServiceWrapper.java
@@ -78,6 +78,16 @@ public class CompanyServiceWrapper
 	}
 
 	@Override
+	public com.liferay.portal.kernel.model.Company addDBPartitionCompany(
+			long companyId, java.lang.String name, java.lang.String virtualHost,
+			java.lang.String webId)
+		throws com.liferay.portal.kernel.exception.PortalException {
+
+		return _companyService.addDBPartitionCompany(
+			companyId, name, virtualHost, webId);
+	}
+
+	@Override
 	public com.liferay.portal.kernel.model.Company deleteCompany(long companyId)
 		throws com.liferay.portal.kernel.exception.PortalException {
 
@@ -415,4 +425,4 @@ public class CompanyServiceWrapper
 	private CompanyService _companyService;
 
 }
-// LIFERAY-SERVICE-BUILDER-HASH:274451923
+// LIFERAY-SERVICE-BUILDER-HASH:-904274314
