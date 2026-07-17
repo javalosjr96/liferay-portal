9eb028888ab504a1b1bee2df9da0e97db7b51b91
Jorge Avalos <jorge.avalos@liferay.com>
2026-07-17 11:15:41 -0700

    LPS-193587 Use raw persistence writes to avoid a masking auto-flush

--- modules/apps/portal/portal-db-partition-test/src/testIntegration/java/com/liferay/portal/db/partition/test/TransactionFlushDBPartitionTest.java
+++ modules/apps/portal/portal-db-partition-test/src/testIntegration/java/com/liferay/portal/db/partition/test/TransactionFlushDBPartitionTest.java
@@ -6,20 +6,20 @@
 package com.liferay.portal.db.partition.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.counter.kernel.service.CounterLocalService;
 import com.liferay.petra.lang.SafeCloseable;
 import com.liferay.portal.db.partition.test.util.BaseDBPartitionTestCase;
 import com.liferay.portal.kernel.dao.orm.DynamicQuery;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.RoleConstants;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
-import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.persistence.RolePersistence;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.TransactionConfig;
 import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
@@ -40,13 +40,6 @@ public class TransactionFlushDBPartitionTest extends BaseDBPartitionTestCase {
 	@BeforeClass
 	public static void setUpClass() throws Exception {
 		_company = CompanyTestUtil.addCompany();
-
-		try (SafeCloseable safeCloseable =
-				CompanyThreadLocal.setCompanyIdWithSafeCloseable(
-					_company.getCompanyId())) {
-
-			_adminUser = UserTestUtil.getAdminUser(_company.getCompanyId());
-		}
 	}
 
 	@AfterClass
@@ -67,10 +60,7 @@ public class TransactionFlushDBPartitionTest extends BaseDBPartitionTestCase {
 						CompanyThreadLocal.setCompanyIdWithSafeCloseable(
 							_company.getCompanyId())) {
 
-					_roleLocalService.addRole(
-						null, _adminUser.getUserId(), null, 0, name, null,
-						null, RoleConstants.TYPE_REGULAR, null,
-						new ServiceContext());
+					_addRole(_company.getCompanyId(), name);
 				}
 
 				return null;
@@ -101,10 +91,7 @@ public class TransactionFlushDBPartitionTest extends BaseDBPartitionTestCase {
 		TransactionInvokerUtil.invoke(
 			_transactionConfig,
 			() -> {
-				_roleLocalService.addRole(
-					null, TestPropsValues.getUserId(), null, 0, name, null,
-					null, RoleConstants.TYPE_REGULAR, null,
-					new ServiceContext());
+				_addRole(TestPropsValues.getCompanyId(), name);
 
 				try (SafeCloseable safeCloseable =
 						CompanyThreadLocal.setCompanyIdWithSafeCloseable(
@@ -144,6 +131,20 @@ public class TransactionFlushDBPartitionTest extends BaseDBPartitionTestCase {
 		}
 	}
 
+	private void _addRole(long companyId, String name) {
+		long roleId = _counterLocalService.increment();
+
+		Role role = _rolePersistence.create(roleId);
+
+		role.setCompanyId(companyId);
+		role.setClassNameId(_classNameLocalService.getClassNameId(Role.class));
+		role.setClassPK(roleId);
+		role.setName(name);
+		role.setType(RoleConstants.TYPE_REGULAR);
+
+		_rolePersistence.update(role);
+	}
+
 	private static final TransactionConfig _transactionConfig;
 
 	static {
@@ -155,10 +156,18 @@ public class TransactionFlushDBPartitionTest extends BaseDBPartitionTestCase {
 		_transactionConfig = builder.build();
 	}
 
-	private static User _adminUser;
 	private static Company _company;
 
 	@Inject
+	private ClassNameLocalService _classNameLocalService;
+
+	@Inject
+	private CounterLocalService _counterLocalService;
+
+	@Inject
 	private RoleLocalService _roleLocalService;
+
+	@Inject
+	private RolePersistence _rolePersistence;
 
 }
