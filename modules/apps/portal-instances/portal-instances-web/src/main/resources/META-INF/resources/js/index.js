78977e0a656290e0084f1b2143c6a08a62a0b257
Jorge Avalos <jorge.avalos@liferay.com>
2026-07-27 14:12:35 -0700

    LPD-92621 Add Import entry to Virtual Instances portlet header

--- modules/apps/portal-instances/portal-instances-web/src/main/resources/META-INF/resources/js/index.js
+++ modules/apps/portal-instances/portal-instances-web/src/main/resources/META-INF/resources/js/index.js
@@ -6,5 +6,6 @@
 export {default as AddInstance} from './AddInstance';
 export {default as CompanyDropdownDefaultPropsTransformer} from './CompanyDropdownDefaultPropsTransformer';
 export {default as CopyInstance} from './CopyInstance';
+export {default as ImportInstance} from './ImportInstance';
 export {default as PortalInstancesManagementToolbarPropsTransformer} from './PortalInstancesManagementToolbarPropsTransformer';
 export {default as openDeleteCompanyModal} from './openDeleteCompanyModal';
