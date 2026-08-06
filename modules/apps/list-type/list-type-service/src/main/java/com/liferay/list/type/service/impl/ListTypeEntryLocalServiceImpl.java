4eb6ad6737d0260dd22c8825aa20bb11968ec0b3
Jorge Avalos <jorge.avalos@liferay.com>
2026-08-06 16:07:39 -0700

    LPD-100559 Reject adding a list type entry across companies
    A list type entry took its company from the acting user while the external reference code uniqueness check was scoped the same way, so users in two different companies could each attach the same external reference code to one definition and neither collided with the other. Requiring the acting user to belong to the definition's company makes the two scopes provably equal and the duplicate detectable.

--- modules/apps/list-type/list-type-service/src/main/java/com/liferay/list/type/service/impl/ListTypeEntryLocalServiceImpl.java
+++ modules/apps/list-type/list-type-service/src/main/java/com/liferay/list/type/service/impl/ListTypeEntryLocalServiceImpl.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
@@ -71,6 +72,10 @@ public class ListTypeEntryLocalServiceImpl
 		}
 
 		User user = _userLocalService.getUser(userId);
+
+		if (user.getCompanyId() != listTypeDefinition.getCompanyId()) {
+			throw new PrincipalException();
+		}
 
 		_validateExternalReferenceCode(
 			externalReferenceCode, user.getCompanyId(), listTypeDefinitionId,
